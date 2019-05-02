0c67ef5e764fc9c67ef6ade99b2cd2f9e44753b7
Ceci Rodriguez <cecilia.rodriguez@base22.com>
2019-05-02 15:25:53 -0500

    Convert to MVC design pattern.

---
+++ 12-Todoey/Data Model/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  12-Todoey
+//
+//  Created by Ceci Rdz on 5/2/19.
+//  Copyright © 2019 Ceci Rdz. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
